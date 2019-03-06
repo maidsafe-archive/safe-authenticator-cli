321a81c272a53ab6c602dc6e36319aef5c94061e
Josh Wilson <joshuef@gmail.com>
2019-03-06 15:55:30 +0100

    tests(cli): auth req tests added

--- tests/cli_integration.rs
+++ tests/cli_integration.rs
@@ -3,11 +3,17 @@ mod cli_integration {
     extern crate rand;
     use rand::distributions::Alphanumeric;
     use rand::{thread_rng, Rng};
+    extern crate predicates;
+    use predicates::prelude::*;
 
     use assert_cmd::prelude::*;
     use std::process::Command;
 
     static UNAUTHED_REQ: &str = "bAAAAAADNVCMIGAQAAAACQAAAAAAAAAAANZSXILTNMFUWI43BMZSS4YLQNFPXA3DBPFTXE33VNZSC453FMJRWY2LFNZ2C4MJQAE";
+    static UNAUTHED_RESPONSE: &str = "bAEAAAADNVCMIGAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAC\n"; // \n added to string with println!
+
+    static AUTHED_REQ: &str = "bAAAAAAFBMHKYWAAAAAABWAAAAAAAAAAANZSXILTNMFUWI43BMZSS45DFON2C453FMJQXA4BONFSAACYAAAAAAAAAABLWKYSBOBYCAVDFON2A2AAAAAAAAAAAJVQWSZCTMFTGKICMORSC4AACAAAAAAAAAAAAUAAAAAAAAAAAL5SG6Y3VNVSW45DTAEAAAAAAAAAAAAIAAAAAOAAAAAAAAAAAL5YHKYTMNFRQCAAAAAAAAAAAAAAAAAAB";
+    static AUTHED_RESPONSE_START: &str = "bAEAAAAFBMHKYW";
 
     #[test]
     fn calling_safe_create_acc() {
@@ -63,6 +69,41 @@ mod cli_integration {
                 &UNAUTHED_REQ,
             ])
             .assert()
+            .stdout(UNAUTHED_RESPONSE)
+            .success();
+    }
+
+    #[test]
+
+    fn calling_safe_auth_with_registered_req() {
+        let rand_string: String = thread_rng().sample_iter(&Alphanumeric).take(30).collect();
+
+        let mut cmd = Command::cargo_bin("safe_auth").unwrap();
+
+        cmd.args(&vec![
+            "--secret",
+            &rand_string,
+            "--password",
+            &rand_string,
+            "--invite-token",
+            "aaa",
+        ])
+        .assert()
+        .success();
+
+        let mut auth_cmd = Command::cargo_bin("safe_auth").unwrap();
+
+        auth_cmd
+            .args(&vec![
+                "--secret",
+                &rand_string,
+                "--password",
+                &rand_string,
+                "-r",
+                &AUTHED_REQ,
+            ])
+            .assert()
+            .stdout(predicate::str::starts_with(AUTHED_RESPONSE_START).from_utf8())
             .success();
     }
 }
