ae68793101c18e63f42cb4bf88c69821d35d5909
bochaco <gabrielviganotti@gmail.com>
2019-03-06 19:43:46 -0300

    feat(CLI): add pretty print arg to the CLI app

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,3 @@
-use log::info;
 use prettytable::Table;
 use safe_auth::{acc_info, authed_apps, authorise_app, create_acc, log_in};
 use structopt::StructOpt;
@@ -24,6 +23,9 @@ pub struct CmdArgs {
     /// Get list of authorised apps
     #[structopt(short = "a", long = "apps")]
     apps: bool,
+    /// Pretty print
+    #[structopt(short = "y", long = "pretty")]
+    pretty: bool,
 }
 
 pub fn run() -> Result<(), String> {
@@ -33,44 +35,79 @@ pub fn run() -> Result<(), String> {
 
     if let Option::Some(invite) = &args.invite {
         create_acc(&invite, &args.secret, &args.password)?;
+        if args.pretty {
+            println!("Account was created successfully!");
+        }
     }
 
     let authenticator = log_in(&args.secret, &args.password)?;
+    if args.pretty {
+        println!("Logged in the SAFE Network successfully!");
+    }
 
     if let Option::Some(req) = &args.req {
         let auth_response = authorise_app(&authenticator, &req)?;
+        if args.pretty {
+            print!("Authorisation response string: ");
+        }
         println!("{}", auth_response);
     }
 
     if args.balance {
         let (mutations_done, mutations_available) = acc_info(&authenticator)?;
-        info!(
-            "Account's current balance (PUTs done/available): {}/{}",
-            mutations_done, mutations_available
-        );
+        if args.pretty {
+            print!("Account's current balance (PUTs done/avaialble): ");
+        }
         println!("{}/{}", mutations_done, mutations_available);
     };
 
     if args.apps {
         let all_apps = authed_apps(&authenticator)?;
-        info!("Authorised applications: {:?}", all_apps);
-
         if !all_apps.is_empty() {
-            table.add_row(row!["Authorised Applications"]);
-            table.add_row(row!["Id", "Name", "Vendor"]);
+            if args.pretty {
+                table.add_row(row!["Authorised Applications"]);
+                table.add_row(row!["Id", "Name", "Vendor", "Permissions"]);
 
-            let all_app_iterator = all_apps.iter();
-
-            for app_info in all_app_iterator {
-                table.add_row(row![
-                    &app_info.app.id,
-                    &app_info.app.name,
-                    // &app_info.app.scope || "",
-                    &app_info.app.vendor,
-                ]);
+                let all_app_iterator = all_apps.iter();
+                for app_info in all_app_iterator {
+                    let mut row = String::from("");
+                    for (cont, perms) in app_info.perms.iter() {
+                        row += &format!("{}: {:?}\n", cont, perms);
+                    }
+                    table.add_row(row![
+                        app_info.app.id,
+                        app_info.app.name,
+                        // app_info.app.scope || "",
+                        app_info.app.vendor,
+                        row,
+                    ]);
+                }
+                table.printstd();
+            } else {
+                println!("APP ID\tNAME\tVENDOR\tPERMISSIONS");
+                let all_app_iterator = all_apps.iter();
+                for app_info in all_app_iterator {
+                    let mut row = format!(
+                        "{}\t{:?}\t{:?}\t[",
+                        &app_info.app.id, &app_info.app.name, &app_info.app.vendor
+                    );
+                    let mut it = app_info.perms.iter();
+                    while let Some((cont, perms)) = it.next() {
+                        row = row + &format!("{:?}:", cont);
+                        let mut it2 = perms.iter();
+                        while let Some(perm) = it2.next() {
+                            row = row + &format!("{:?}", perm);
+                            if it2.size_hint().0 > 0 {
+                                row += "|";
+                            };
+                        }
+                        if it.size_hint().0 > 0 {
+                            row += ",";
+                        };
+                    }
+                    println!("{}]", row)
+                }
             }
-
-            table.printstd();
         }
     };
 
