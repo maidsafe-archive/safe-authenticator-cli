b237db88a50ff2795d3670565ef94d24a496da4e
Josh Wilson <joshuef@gmail.com>
2019-03-06 10:05:32 +0100

    feat(CLI): Remove subcommands. Make some flags optional.
    Remove abundance of !! from git hook.

--- src/cli.rs
+++ src/cli.rs
@@ -1,39 +1,24 @@
 use log::info;
-use std::error::Error;
 
 use safe_auth::{acc_info, authed_apps, authorise_app, create_acc, log_in};
 
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-pub enum SubCommands {
-    #[structopt(name = "create")]
-    /// Create a new SAFE Network account with the credentials provided
-    Invite {
-        /// The invitation token for creating a new SAFE Network account
-        #[structopt(short = "i", long = "invite-token")]
-        invite: String,
-    },
-    #[structopt(name = "auth")]
-    /// Authorise an application by providing the authorisation request URI or string
-    Auth {
-        /// The authorisation request URI or string
-        #[structopt(short = "r", long = "req")]
-        req: String,
-    },
-}
-
-#[derive(StructOpt, Debug)]
+/// Manage SAFE Network authorisations and accounts.
 pub struct CmdArgs {
+    /// The authorisation request URI or string
+    #[structopt(short = "r", long = "req")]
+    req: Option<String>,
+    /// The invitation token for creating a new SAFE Network account
+    #[structopt(short = "i", long = "invite-token")]
+    invite: Option<String>,
     /// The secret phrase of the SAFE account
     #[structopt(short = "s", long = "secret")]
     secret: String,
     /// The SAFE account's password
     #[structopt(short = "p", long = "password")]
     password: String,
-    /// subcommands
-    #[structopt(subcommand)]
-    cmd: Option<SubCommands>,
     /// Get account's balance
     #[structopt(short = "b", long = "balance")]
     balance: bool,
@@ -42,27 +27,18 @@ pub struct CmdArgs {
     apps: bool,
 }
 
-pub fn run() -> Result<(), Box<dyn Error>> {
+pub fn run() -> Result<(), String> {
     let args = CmdArgs::from_args();
 
-    let authenticator = match args.cmd {
-        None => log_in(&args.secret, &args.password).and_then(|auth| {
-            info!("Logged-in successfully!");
-            Ok(auth)
-        })?,
-        Some(SubCommands::Invite { invite }) => create_acc(&invite, &args.secret, &args.password)
-            .and_then(|auth| {
-            info!("Account created successfully!");
-            Ok(auth)
-        })?,
-        Some(SubCommands::Auth { req }) => {
-            log_in(&args.secret, &args.password).and_then(|auth| {
-                let resp = authorise_app(&auth, &req)?;
-                info!("Auth response generated: {:?}", resp);
-                Ok(auth)
-            })?
-        }
-    };
+    if let Option::Some(invite) = &args.invite {
+        create_acc(&invite, &args.secret, &args.password)?;
+    }
+
+    let authenticator = log_in(&args.secret, &args.password)?;
+
+    if let Option::Some(req) = &args.req {
+        authorise_app(&authenticator, &req)?;
+    }
 
     if args.balance {
         let (mutations_done, mutations_available) = acc_info(&authenticator)?;
