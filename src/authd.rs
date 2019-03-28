a639b823ba64217093118f97ab6338cdcae3d2ec
Stephen Coyle <stephen.coyle@maidsafe.net>
2019-03-28 13:36:25 +0000

    chore(license): LICENSE update

--- src/authd.rs
+++ src/authd.rs
@@ -1,3 +1,11 @@
+// Copyright 2019 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. Please review the Licences for the specific language governing
+// permissions and limitations relating to use of the SAFE Network Software.
+
 use super::{authorise_app, create_acc, log_in};
 use actix_web::{actix::*, http::Method, server, ws, App, Error, HttpRequest, HttpResponse, Path};
 use safe_authenticator::{AuthError, Authenticator};
