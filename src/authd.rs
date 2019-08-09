e02703001ba6b7e8bf30bb92592a4dd36a2fd991
Gabriel Viganotti <gabrielviganotti@gmail.com>
2019-08-09 19:21:08 -0300

    test(authd): adapt tests to use new actix-web lib

--- src/authd.rs
+++ src/authd.rs
@@ -7,7 +7,7 @@
 // permissions and limitations relating to use of the SAFE Network Software.
 
 use actix_web::{web, App, HttpResponse, HttpServer};
-use safe_auth::{authorise_app, create_acc, log_in, AuthAllowPrompt};
+use safe_auth::{authorise_app, /*create_acc, log_in,*/ AuthAllowPrompt};
 use safe_authenticator::{AuthError, Authenticator};
 use std::sync::{Arc, Mutex};
 
@@ -33,20 +33,27 @@ pub fn run(
                 handle: handle.clone(),
                 allow_auth_cb: Arc::new(prompt_to_allow),
             })
-            .route(
-                "/",
-                web::get().to(|| {
-                    HttpResponse::Ok().body("SAFE Authenticator service is up and running!")
-                }),
-            )
-            .route("/authorise/{auth_req}", web::get().to(authd_authorise))
-            .default_service(
-                web::get().to(|| HttpResponse::NotFound().body("Service endpoint not found.")),
-            )
+            .configure(configure_services)
     })
     .bind(&address)
     .unwrap()
     .run();
+}
+
+fn configure_services(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::resource("/").route(
+        web::get().to(|| HttpResponse::Ok().body("SAFE Authenticator service is up and running!")),
+    ));
+    cfg.service(
+        web::resource("/create/{secret}/{password}/{invite}")
+            .route(web::post().to(authd_create_acc)),
+    );
+    cfg.service(web::resource("/login/{secret}/{password}").route(web::post().to(authd_login)));
+    cfg.service(web::resource("/authorise/{auth_req}").route(web::get().to(authd_authorise)));
+    cfg.service(
+        web::resource("*")
+            .route(web::get().to(|| HttpResponse::NotFound().body("Service endpoint not found."))),
+    );
 }
 
 struct AuthenticatorState {
@@ -54,11 +61,12 @@ struct AuthenticatorState {
     pub allow_auth_cb: Arc<&'static AuthAllowPrompt>,
 }
 
-#[allow(dead_code)]
 fn authd_create_acc(
-    info: web::Path<(String, String, String)>,
-    req: web::Data<AuthenticatorState>,
+    _info: web::Path<(String, String, String)>,
+    _req: web::Data<AuthenticatorState>,
 ) -> HttpResponse {
+    HttpResponse::NotFound().body("Create service not supported yet.")
+    /*
     match create_acc(&info.2.clone(), &info.0.clone(), &info.1.clone()) {
         Ok(auth) => {
             *(unwrap!(req.handle.lock())) = Some(Ok(auth));
@@ -70,13 +78,15 @@ fn authd_create_acc(
             HttpResponse::BadRequest().body(response_string)
         }
     }
+    */
 }
 
-#[allow(dead_code)]
 fn authd_login(
-    info: web::Path<(String, String)>,
-    req: web::Data<AuthenticatorState>,
+    _info: web::Path<(String, String)>,
+    _req: web::Data<AuthenticatorState>,
 ) -> HttpResponse {
+    HttpResponse::NotFound().body("Login service not supported yet.")
+    /*
     match log_in(&info.0.clone(), &info.1.clone()) {
         Ok(auth) => {
             *(unwrap!(req.handle.lock())) = Some(Ok(auth));
@@ -88,6 +98,7 @@ fn authd_login(
             HttpResponse::BadRequest().body(response_string)
         }
     }
+    */
 }
 
 fn authd_authorise(
@@ -115,87 +126,83 @@ fn authd_authorise(
 
 #[cfg(test)]
 mod tests {
-    use super::{create_acc, create_web_service, AuthenticatorState, SharedHandleType};
-    use actix_web::{http::Method, test, ws, HttpMessage};
-    use futures::Stream;
+    use super::{configure_services, AuthenticatorState};
+    use actix_web::{test, App};
     use rand::Rng;
-    use safe_authenticator::Authenticator;
+    use safe_auth::create_acc;
     use std::str::from_utf8;
     use std::sync::{Arc, Mutex};
 
-    fn create_test_service(authenticator: Option<Authenticator>) -> test::TestServer {
-        let handle: SharedHandleType = match authenticator {
-            Some(auth) => Arc::new(Mutex::new(Some(Ok(auth)))),
-            None => Arc::new(Mutex::new(None)),
+    macro_rules! create_test_service {
+        ($authenticator:expr) => {
+            test::init_service(
+                App::new()
+                    .data(AuthenticatorState {
+                        handle: match $authenticator {
+                            Some(auth) => Arc::new(Mutex::new(Some(Ok(auth)))),
+                            None => Arc::new(Mutex::new(None)),
+                        },
+                        allow_auth_cb: Arc::new(&|_| true),
+                    })
+                    .configure(configure_services),
+            )
         };
-        test::TestServer::with_factory(move || {
-            create_web_service(AuthenticatorState {
-                handle: handle.clone(),
-                allow_auth_cb: Arc::new(&|_| true),
-            })
-        })
     }
 
     #[test]
-    fn get_index() {
-        let mut srv = create_test_service(None);
-        let request = unwrap!(srv.client(Method::GET, "/").finish());
-        let response = unwrap!(srv.execute(request.send()));
-
-        assert!(response.status().is_success());
+    fn get_root() {
+        let mut srv = create_test_service!(None);
+        let request = test::TestRequest::get().uri("/").to_request();
+        let response = test::read_response(&mut srv, request);
+        let body = unwrap!(from_utf8(&response));
+        assert_eq!(body, "SAFE Authenticator service is up and running!");
     }
 
     #[test]
-    #[ignore] // we don't expose create acc from webservice yet
+    fn get_invalid_endpoint() {
+        let mut srv = create_test_service!(None);
+        let request = test::TestRequest::get()
+            .uri("/invalid-endpoint")
+            .to_request();
+        let response = test::read_response(&mut srv, request);
+        let body = unwrap!(from_utf8(&response));
+        assert_eq!(body, "Service endpoint not found.");
+    }
+
+    #[test] // we don't expose create acc from webservice yet
     fn post_create_account() {
         let mut rng = rand::thread_rng();
         let secret: u32 = rng.gen();
         let password: u32 = rng.gen();
         let invite: u16 = rng.gen();
-        let mut srv = create_test_service(None);
-
+        let mut srv = create_test_service!(None);
         let endpoint = format!("/create/{}/{}/{}", secret, password, invite);
-        let request = unwrap!(srv.client(Method::POST, &endpoint).finish());
-        match srv.execute(request.send()) {
-            Ok(response) => {
-                assert!(response.status().is_success());
-            }
-            Err(req_err) => {
-                println!("POST create account request error: {:?}", req_err);
-            }
-        }
+        let request = test::TestRequest::post().uri(&endpoint).to_request();
+        let response = test::read_response(&mut srv, request);
+        let body = unwrap!(from_utf8(&response));
+        assert_eq!(body, "Create service not supported yet.");
     }
 
-    #[test]
-    #[ignore] // we don't expose login from webservice yet
+    #[test] // we don't expose login from webservice yet
     fn post_login() {
         let mut rng = rand::thread_rng();
         let secret: u32 = rng.gen();
         let password: u32 = rng.gen();
         let invite: u16 = rng.gen();
-        let mut srv = create_test_service(None);
+        let mut srv = create_test_service!(None);
         let create_acc_endpoint = format!("/create/{}/{}/{}", secret, password, invite);
-        let create_acc_request = unwrap!(srv.client(Method::POST, &create_acc_endpoint).finish());
-        match srv.execute(create_acc_request.send()) {
-            Ok(response) => {
-                assert!(response.status().is_success());
-            }
-            Err(req_err) => {
-                println!("POST create account error: {:?}", req_err);
-            }
-        }
+        let request = test::TestRequest::post()
+            .uri(&create_acc_endpoint)
+            .to_request();
+        let response = test::read_response(&mut srv, request);
+        let body = unwrap!(from_utf8(&response));
+        assert_eq!(body, "Create service not supported yet.");
 
         let login_endpoint = format!("/login/{}/{}", secret, password);
-        let login_request = unwrap!(srv.client(Method::POST, &login_endpoint).finish());
-
-        match srv.execute(login_request.send()) {
-            Ok(response) => {
-                assert!(response.status().is_success());
-            }
-            Err(req_err) => {
-                println!("POST login request error: {:?}", req_err);
-            }
-        }
+        let request = test::TestRequest::post().uri(&login_endpoint).to_request();
+        let response = test::read_response(&mut srv, request);
+        let body = unwrap!(from_utf8(&response));
+        assert_eq!(body, "Login service not supported yet.");
     }
 
     #[test]
@@ -207,29 +214,12 @@ mod tests {
         let secret = &(random_str());
         let password = &(random_str());
         let authenticator = unwrap!(create_acc(invite, secret, password));
-        let mut srv = create_test_service(Some(authenticator));
+
+        let mut srv = create_test_service!(Some(authenticator));
         let endpoint = "/authorise/bAAAAAAEXVK4SGAAAAAABAAAAAAAAAAAANZSXILTNMFUWI43BMZSS4Y3MNEAAQAAAAAAAAAAAKNAUMRJAINGESEAAAAAAAAAAABGWC2LEKNQWMZJONZSXIICMORSAAAIBAAAAAAAAAAAAOAAAAAAAAAAAL5YHKYTMNFRQCAAAAAAAAAAAAAAAAAAB";
-        let request = unwrap!(srv.client(Method::GET, &endpoint).finish());
-        match srv.execute(request.send()) {
-            Ok(response) => {
-                assert!(response.status().is_success());
-                let bytes = unwrap!(srv.execute(response.body()));
-                let body = unwrap!(from_utf8(&bytes));
-                assert!(body.len() > 0);
-            }
-            Err(req_err) => {
-                println!("GET authorise request error: {:?}", req_err);
-            }
-        }
-    }
-
-    #[test]
-    fn get_web_socket() {
-        let mut srv = create_test_service(None);
-        let (reader, mut writer) = unwrap!(srv.ws_at("/ws"));
-        writer.text("text");
-
-        let (item, _reader) = unwrap!(srv.execute(reader.into_future()));
-        assert_eq!(item, Some(ws::Message::Text("text".to_owned())));
+        let request = test::TestRequest::get().uri(&endpoint).to_request();
+        let response = test::read_response(&mut srv, request);
+        let body = unwrap!(from_utf8(&response));
+        assert!(body.len() > 0);
     }
 }
